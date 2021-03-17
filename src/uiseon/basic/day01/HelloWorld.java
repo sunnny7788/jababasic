60ba86093cd22ac780f202370936da4779237bcc
sunny7788 <uiseon0309@gmail.com>
2021-03-17 12:53:54 +0900

    Hello 변수선언 변수명지정 산술연산자 출력문

---
+++ src/uiseon/basic/day01/HelloWorld.java
@@ -0,0 +1,7 @@
+package uiseon.basic.day01;
+
+public class HelloWorld {
+  public static void main(String[] args0 ) {
+    System.out.println("Hello, WORLD!!");
+ }
+}
---
+++ src/uiseon/basic/day01/ex05.java
@@ -0,0 +1,16 @@
+package uiseon.basic.day01;
+
+public class ex05 {
+	public static void main(String [] args0) {
+	   //강제형변환 예시  
+	double number=(1/3)*3;
+	int pi2=(int);
+	
+	
+			
+   
+   
+   
+   
+   }
+}
